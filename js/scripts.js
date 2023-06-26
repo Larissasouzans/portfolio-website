3fa6bfe709102f16db42c359038b65db855821dc
Larissasouzans <131874382+Larissasouzans@users.noreply.github.com>
2023-06-26 20:59:30 +0200

    Update scripts.js
    If applied, this commit will add a hamburger menu to header

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,10 @@
 console.log("Hello! Welcome to My Portfolio Site. It's nice to see you here.")
+
+function menuToggle() {
+    var x = document.getElementById('myNavtoggle');
+    if (x.className === 'navtoggle') {
+      x.className += ' responsive';
+    } else {
+      x.className = 'navtoggle';
+    }
+  }

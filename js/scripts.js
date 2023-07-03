9977e7659633aaf4074efdc876ddef16e7597d87
Larissasouzans <131874382+Larissasouzans@users.noreply.github.com>
2023-07-03 22:52:40 +0200

    Update scripts.js
    If applied, this commit will correct header problem by contact in bigger screans

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@ console.log("Hello! Welcome to My Portfolio Site. It's nice to see you here.")
 
 function menuToggle() {
     var x = document.getElementById('myNavtoggle');
-    if (x.className === 'navtoggle') {
+    if (x.className === 'navtoggle' && $(window).width() < 640) {
       x.className += ' responsive';
     } else {
       x.className = 'navtoggle';
